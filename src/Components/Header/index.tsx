5cde92190e5b469aea3e2a222a73e6388075a806
yunjjeongjo <two6885@gmail.com>
2022-11-27 00:03:11 +0900

    feat: Header 구현

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return <Wrapper>Angular/Angular-cli</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+export default Header;
