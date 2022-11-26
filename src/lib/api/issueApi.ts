d529c06900e77dfa6c21817cf37c00021de10c28
yunjjeongjo <two6885@gmail.com>
2022-11-27 00:00:58 +0900

    feat: getIssueList, getIssue api 구현

---
+++ src/lib/api/issueApi.ts
@@ -0,0 +1,27 @@
+import { Octokit } from "@octokit/rest";
+const { REACT_APP_GITHUB_TOKEN } = process.env;
+
+const octokit = new Octokit({
+  auth: REACT_APP_GITHUB_TOKEN
+});
+
+export const getIssueList = async (pageNumber: number) => {
+  const { data } = await octokit.rest.issues.listForRepo({
+    owner: "angular",
+    repo: "angular-cli",
+    state: "open",
+    sort: "comments",
+    page: pageNumber,
+    per_page: 10
+  });
+  return data;
+};
+
+export const getIssue = async (issueNumber: number) => {
+  const { data } = await octokit.rest.issues.get({
+    owner: "angular",
+    repo: "angular-cli",
+    issue_number: issueNumber
+  });
+  return data;
+};
