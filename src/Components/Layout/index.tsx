273131bdc1d65571bf93e0d83ea9e83f760c3db8
yunjjeongjo <two6885@gmail.com>
2022-11-28 21:52:51 +0900

    design: 레이아웃 min-width 추가

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -19,9 +19,9 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   min-height: 100vh;
+  min-width: 450px;
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
   background-color: #e4e4e4;
 `;
