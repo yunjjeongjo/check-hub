5e064c14295944c668b7e3a359e5e8bebff33670
yunjjeongjo <two6885@gmail.com>
2022-11-29 02:48:07 +0900

    desgin: 헤더 box-shadow 속성 추가

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -6,11 +6,12 @@ const Header = () => {
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 50px;
+  height: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: white;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
 export default Header;
