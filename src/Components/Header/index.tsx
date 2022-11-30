1a0d96bf68891767582852741e3bbc6054a9a0c9
yunjjeongjo <two6885@gmail.com>
2022-11-30 23:32:22 +0900

    feat: 페이지 라우팅 변경

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,40 @@
 import styled from "styled-components";
+import { FaChevronLeft } from "react-icons/fa";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Header = () => {
-  return <Wrapper>Angular/Angular-cli</Wrapper>;
+  const navigate = useNavigate();
+  const { owner, repo } = useParams();
+  const handelClick = () => {
+    navigate(-1);
+  };
+  return (
+    <Wrapper>
+      <GoBackIcon onClick={handelClick} />
+      <Title>
+        {owner} / {repo}
+      </Title>
+    </Wrapper>
+  );
 };
 
+const GoBackIcon = styled(FaChevronLeft)`
+  position: absolute;
+  left: 40px;
+  cursor: pointer;
+  color: #1947e5;
+`;
+
+const Title = styled.div`
+  font-weight: 500;
+  font-size: 18px;
+  border-bottom: 3px #bec5d9 solid;
+`;
+
 const Wrapper = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 10;
   width: 100%;
   height: 80px;
   display: flex;
--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,13 +1,32 @@
+import Header from "@/Components/Header";
 import DetailPage from "@/Pages/DetailPage";
 import MainPage from "@/Pages/MainPage";
 import NotFound from "@/Pages/NotFound";
+import SearchPage from "@/Pages/SearchPage";
 import { Route, Routes } from "react-router-dom";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route
+        path="/:owner/:repo"
+        element={
+          <>
+            <Header />
+            <MainPage />
+          </>
+        }
+      />
+      <Route path="/" element={<SearchPage />} />
+      <Route
+        path="/:owner/:repo/detail/:id"
+        element={
+          <>
+            <Header />
+            <DetailPage />
+          </>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
