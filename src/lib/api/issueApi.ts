20fb6f49005a59caa15b0fe6541efcac1a362c2f
yunjjeongjo <two6885@gmail.com>
2022-12-04 18:24:23 +0900

    feat: issueApi 수정

--- src/lib/api/issueApi.ts
+++ src/lib/api/issueApi.ts
@@ -5,22 +5,44 @@ const octokit = new Octokit({
   auth: REACT_APP_GITHUB_TOKEN
 });
 
-export const getIssueList = async (pageNumber: number) => {
-  const { data } = await octokit.rest.issues.listForRepo({
-    owner: "angular",
-    repo: "angular-cli",
-    state: "open",
-    sort: "comments",
-    page: pageNumber,
-    per_page: 20
-  });
-  return data;
+export const getIssueList = async (
+  pageNumber: number,
+  owner: string,
+  repo: string,
+  state?: any,
+  sort?: any
+) => {
+  // console.log(state, sort);
+  if (
+    (state === "open" ||
+      state === "closed" ||
+      state === "all" ||
+      state === undefined) &&
+    (sort === "comments" ||
+      sort === "created" ||
+      sort === "updated" ||
+      sort === undefined)
+  ) {
+    const { data } = await octokit.rest.issues.listForRepo({
+      owner: owner,
+      repo: repo,
+      state: state,
+      sort: sort,
+      page: pageNumber,
+      per_page: 20
+    });
+    return data;
+  }
 };
 
-export const getIssue = async (issueNumber: number) => {
+export const getIssue = async (
+  issueNumber: number,
+  owner: string,
+  repo: string
+) => {
   const { data } = await octokit.rest.issues.get({
-    owner: "angular",
-    repo: "angular-cli",
+    owner: owner,
+    repo: repo,
     issue_number: issueNumber
   });
   return data;
