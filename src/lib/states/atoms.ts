4ffd193b1d73e14d09611bc314ed716463d7c584
yunjjeongjo <two6885@gmail.com>
2022-12-04 18:25:34 +0900

    feat: issueState recoil-perdsist 추가

--- src/lib/states/atoms.ts
+++ src/lib/states/atoms.ts
@@ -1,6 +1,20 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const issueState = atom({
   key: "issueState",
-  default: { owner: "", repo: "" }
+  default: { owner: "", repo: "" },
+  effects_UNSTABLE: [persistAtom]
+});
+
+export const issueActiveState = atom({
+  key: "issueActiveState",
+  default: "open"
+});
+
+export const issueSortState = atom({
+  key: "issueSortState",
+  default: "comments"
 });
